52c64dfaa1274a4f1a55e0f1a1436feef866248c
SoroushBeigi <sbeygi712@gmail.com>
2024-01-02 17:01:08 +0330

    adding dependency injections

--- lib/injections.dart
+++ lib/injections.dart
@@ -1,8 +1,14 @@
 import 'package:get_it/get_it.dart';
 import 'package:notes/notes_feature/data/local/database_provider.dart';
+import 'package:notes/notes_feature/domain/repositories/db_repository.dart';
 
 GetIt locator = GetIt.I;
 
 setup() async {
 
+  //providers
+  locator.registerSingleton<DataBaseProvider>(DataBaseProvider(boxName: 'box'));
+
+  //repositories
+  locator.registerSingleton<DataBaseRepository>(DataBaseRepository(boxName: 'box',dataBaseProvider: locator()));
 }
